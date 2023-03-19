dd8b080ecf1fbd81be639ccae20824759dafde49
Leftjose <87351665+Leftjose@users.noreply.github.com>
2023-03-18 20:40:57 -0500

    adding wallet connect button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import './App.css';
 
 
 function App() {
-  return (
-    <h1>Hello World</h1>
+    const connectWallet = async() => {
+      if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+        try {
+          // metamask is installed
+          const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
+          console.log(accounts[0]);
+        }catch(err) {
+          console.error(err.message);
+        }
+      } else {
+        console.log("Please install metamask");
+      }
+    }
+
+
+    return (
+    <button onClick={connectWallet}>Connect Wallet</button>
   );
 }
+
+
+
+
+
+
+
 
 export default App;
